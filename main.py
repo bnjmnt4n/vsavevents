a5aa55b437602d0e5c888e875db086e63b74edd4
Benjamin Tan <demoneaux@gmail.com>
2013-06-28 22:26:31 +0800

    appengine default stuff

---
+++ main.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+import webapp2
+import os
+import jinja2
+
+JINJA_ENVIRONMENT = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
+    extensions=['jinja2.ext.autoescape'])
+
+class MainHandler(webapp2.RequestHandler):
+    def get(self):
+        self.response.write('Hello world!')
+
+app = webapp2.WSGIApplication([
+    ('/', MainHandler)
+], debug=True)
