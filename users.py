831d15ad3a9fe46be01e791eca46660ecb384d77
wei2912 <wei2912_support@hotmail.com>
2013-07-01 23:01:48 +0800

    Add support email

--- users.py
+++ users.py
@@ -1,6 +1,6 @@
 from google.appengine.api import users
 
-users_string = '||demoneaux@gmail.com|weien1292@gmail.com|'
+users_string = '|demoneaux@gmail.com|weien1292@gmail.com|wei2912.supp0rt@gmail.com|'
 
 def get_user():
 	current_user = users.get_current_user()
