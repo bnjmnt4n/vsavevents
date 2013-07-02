62f3c68935672ea1ea8e45cba413d2558a984048
wei2912 <wei2912_support@hotmail.com>
2013-07-02 19:49:12 +0800

    Mail bugfix

--- mail.py
+++ mail.py
@@ -6,7 +6,11 @@ import parser
 class LogSenderHandler(InboundMailHandler):
     def receive(self, mail_message):
         logging.info("Received a message from: " + mail_message.sender)
-        message = mail_message.bodies('text/plain')
-        parser.parse(message)
+        plaintext = mail_message.bodies('text/plain')
+        
+        for text in plaintext:
+            txtmsg = ""
+            txtmsg = text[1].decode()
+            parser.parse(txtmsg)
         
 app = webapp2.WSGIApplication([LogSenderHandler.mapping()], debug=True)
