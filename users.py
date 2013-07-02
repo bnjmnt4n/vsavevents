5ef9588e9f0741394e068c5d19fddeeeba78e11d
wei2912 <wei2912_support@hotmail.com>
2013-07-02 16:45:09 +0800

    Bugfix

--- users.py
+++ users.py
@@ -4,7 +4,7 @@ users_string = '|demoneaux@gmail.com|weien1292@gmail.com|wei2912.supp0rt@gmail.c
 
 def get_user():
 	current_user = users.get_current_user()
-	if users_string.find('|' + current_user.email() + '|'):
+	if users_string.find('|' + current_user.email() + '|') != -1:
 		return current_user
 	else:
 		return False
