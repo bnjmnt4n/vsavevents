bcf3be95388c1b6792131ab777e3630c528ef4c9
wei2912 <wei2912.supp0rt@gmail.com>
2013-09-03 20:46:26 +0800

    Add automatic old & new parser selection when receiving mail

--- mail.py
+++ mail.py
@@ -13,15 +13,16 @@ class LogSenderHandler(InboundMailHandler):
         text_bodies = mail_message.bodies('text/plain')
         html_bodies = mail_message.bodies('text/html')
 
-        if sender in ("vsavict@gmail.com", "demoneaux@gmail.com", "weien1292@gmail.com", "wei2912.supp0rt@gmail.com"):
-            for text in html_bodies:
-                txt = text[1].decode()
-                logging.info(txt)
-                new_format.parse(txt)
-        elif sender in ("webmaster@vs.moe.edu.sg"): # to be shifted to new format soon
-            for text in text_bodies:
-                txt = text[1].decode()
-                logging.info(txt)
-                old_format.parse(txt)
+        if sender in ("vsavict@gmail.com", "demoneaux@gmail.com", "weien1292@gmail.com", "wei2912.supp0rt@gmail.com", "webmaster@vs.moe.edu.sg"):
+            if "[WORK ORDER]" in subject:
+                for text in html_bodies:
+                    txt = text[1].decode()
+                    logging.info(txt)
+                    new_format.parse(txt)
+            elif "Audio Visual Department - Work Order Form" in subject:
+                for text in text_bodies:
+                    txt = text[1].decode()
+                    logging.info(txt)
+                    old_format.parse(txt)
         
 app = webapp2.WSGIApplication([LogSenderHandler.mapping()], debug=True)
