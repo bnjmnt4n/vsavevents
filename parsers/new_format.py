98745580d99af805a24c1eeb66d2883186aa5350
wei2912 <wei2912.supp0rt@gmail.com>
2013-10-12 16:01:25 +0800

    Add space removal

--- parsers/new_format.py
+++ parsers/new_format.py
@@ -7,7 +7,8 @@ from google.appengine.ext import ndb
 from utils import html
 
 def parse(msg):
-    msg = html.strip_tags(msg).strip().split("\n")
+    msg = html.strip_tags(msg).split("\n")
+    msg = [i.strip() for i in msg]
     for i in range(len(msg)):
         if msg[i][-1:] == ":":
             msg[i] = msg[i] + " " # add a space to the colon; dirty hack
