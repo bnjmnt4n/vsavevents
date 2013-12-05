2a5302be07098187cb216e110ba556f5b6016ff3
Wei En <wei2912.supp0rt@gmail.com>
2013-12-05 16:23:24 +0800

    Removed indexed=False as it prevents editing of value in DB.

--- models.py
+++ models.py
@@ -6,15 +6,13 @@ class Event(ndb.Model):
 	start_time = ndb.TimeProperty()
 	end_time = ndb.TimeProperty()
 	
-	teacher = ndb.StringProperty(indexed=False)
-	location = ndb.StringProperty(indexed=False)
-	levels = ndb.StringProperty(indexed=False)
-	equipment = ndb.StringProperty(indexed=False)
-	remarks = ndb.StringProperty(indexed=False)
+	teacher = ndb.StringProperty()
+	location = ndb.StringProperty()
+	levels = ndb.StringProperty()
+	equipment = ndb.StringProperty()
+	remarks = ndb.StringProperty()
 
 class User(ndb.Model):
 	name = ndb.StringProperty()
 	email = ndb.StringProperty()
-	level = ndb.IntegerProperty(indexed=False)
-	# level 1 - mortal
-	# level 2 - immortal admin
+	level = ndb.IntegerProperty()
