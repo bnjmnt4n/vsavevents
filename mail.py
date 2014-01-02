a92f105aa10aee0880ec00013ceec4b1726d7f1b
Wei En <wei2912.supp0rt@gmail.com>
2014-01-02 22:54:31 +0800

    Fix...

--- mail.py
+++ mail.py
@@ -9,23 +9,24 @@ class LogSenderHandler(InboundMailHandler):
         sender = mail_message.sender
         subject = mail_message.subject
 
+        if "<" in sender and ">" in sender:
+            sender = sender.split("<")[1].split(">")[0]
+
         logging.info("%s: %s" % (sender, subject))
 
         text_bodies = mail_message.bodies('text/plain')
         html_bodies = mail_message.bodies('text/html')
 
-        regex = re.match("[\w\d\.]+@[\w\d]+\.[\w\d]+", sender)
-        if regex:
-            if sender in ("vsavict@gmail.com", "demoneaux@gmail.com", "weien1292@gmail.com", "wei2912.supp0rt@gmail.com", "webmaster@vs.moe.edu.sg"):
-                if "[WORK ORDER]" in subject:
-                    for text in html_bodies:
-                        txt = text[1].decode()
-                        logging.info(txt)
-                        new_format.parse(txt)
-                elif "Audio Visual Department - Work Order Form" in subject:
-                    for text in text_bodies:
-                        txt = text[1].decode()
-                        logging.info(txt)
-                        old_format.parse(txt)
+        if sender in ("vsavict@gmail.com", "demoneaux@gmail.com", "weien1292@gmail.com", "wei2912.supp0rt@gmail.com", "webmaster@vs.moe.edu.sg"):
+            if "[WORK ORDER]" in subject:
+                for text in html_bodies:
+                    txt = text[1].decode()
+                    logging.info(txt)
+                    new_format.parse(txt)
+            elif "Audio Visual Department - Work Order Form" in subject:
+                for text in text_bodies:
+                    txt = text[1].decode()
+                    logging.info(txt)
+                    old_format.parse(txt)
         
 app = webapp2.WSGIApplication([LogSenderHandler.mapping()], debug=True)
